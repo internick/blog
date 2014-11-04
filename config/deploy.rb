8e19d2190c950dda70023b7a26882e39fa582b48
Nick Cochran <internick@gmail.com>
2014-11-03 21:51:06 -0600

    more testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,40 @@
-require 'bundler/capistrano'
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
-set :application, "blog"
-set :repository,  "git@github.com:internick/blog.git"
-set :deploy_to, "/var/www/blog.internick.com"
+set :application, 'blog'
+set :repo_url, 'git@github.com:internick/blog.git'
+set :rbenv_ruby, '2.1.3'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, 'master'
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/var/www/blog.internick.com'
+
+# Default value for :scm is :git
 set :scm, :git
-set :branch, "master"
-set :user, "internick"
-set :group, "deployers"
-set :use_sudo, false
-set :rails_env, "production"
-set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true }
-set :keep_releases, 5
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
 set :pty, true
-server "104.200.16.18", :app, :web, :db, :primary => true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system }
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
 
 namespace :deploy do
   task :start do ; end
@@ -39,6 +60,6 @@ namespace :deploy do
   end
 end
 
-after "deploy", "deploy:symlink_config_files"
+after "updating", "deploy:symlink_config_files"
 after "deploy", "deploy:restart"
 after "deploy", "deploy:cleanup"
