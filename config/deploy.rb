ecc37b16e00cb241e2020458fc7e1b27eaa7138d
Nick Cochran <internick@gmail.com>
2014-11-03 20:11:29 -0600

    Another capistrano fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,44 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+require 'bundler/capistrano'
 
-set :application, 'blog'
-set :repo_url, 'git@github.com:internick/blog.git'
-set :rbenv_ruby, '2.1.3'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'master'
-
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/blog.internick.com'
-
-# Default value for :scm is :git
+set :application, "blog"
+set :repository,  "git@github.com:internick/blog.git"
+set :deploy_to, "/var/www/blog.internick.com"
 set :scm, :git
-
-# Default value for :format is :pretty
-set :format, :pretty
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
-# Default value for :pty is false
-set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
+set :branch, "master"
+set :user, "internick"
+set :group, "deployers"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true }
 set :keep_releases, 5
+set :pty, true
+server "104.200.16.18", :app, :web, :db, :primary => true
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  task :start do ; end
+  task :stop do ; end
 
   desc "Symlink shared config files"
   task :symlink_config_files do
-    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
 
-  after "deploy", "deploy:symlink_config_files"
-  after :publishing, :restart
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  #desc "Precompile assets after deploy"
+  #task :precompile_assets do
+  #  run <<-CMD
+  #    cd #{ current_path } &&
+  #    #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+  #  CMD
+  #end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
   end
-
 end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
