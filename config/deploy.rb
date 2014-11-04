27a01340983325461d493c155e0f97a52dfb2b4b
Nick Cochran <internick@gmail.com>
2014-11-03 19:54:09 -0600

    troubleshooting capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,12 @@ namespace :deploy do
     end
   end
 
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
+  after "deploy", "deploy:symlink_config_files"
   after :publishing, :restart
 
   after :restart, :clear_cache do
